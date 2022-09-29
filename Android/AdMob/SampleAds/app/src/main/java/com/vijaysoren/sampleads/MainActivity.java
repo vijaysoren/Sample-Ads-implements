3dff5aa9023dcd8d511e182b5915f32c27f19182
vijaysoren <37494793+vijaysoren@users.noreply.github.com>
2022-09-29 12:29:10 +0530

    Add files via upload

---
+++ Android/AdMob/SampleAds/app/src/main/java/com/vijaysoren/sampleads/MainActivity.java
@@ -0,0 +1,36 @@
+package com.vijaysoren.sampleads;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+
+import com.google.android.gms.ads.MobileAds;
+
+import com.google.android.gms.ads.initialization.InitializationStatus;
+import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
+import com.google.android.gms.ads.interstitial.InterstitialAd;
+
+public class MainActivity extends AppCompatActivity {
+
+    private InterstitialAd mInterstitialAd;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        MobileAds.initialize(this, new OnInitializationCompleteListener() {
+            @Override
+            public void onInitializationComplete(InitializationStatus initializationStatus) {
+            }
+        });
+
+        AdView adView = findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        adView.loadAd(adRequest);
+
+    }
+}
