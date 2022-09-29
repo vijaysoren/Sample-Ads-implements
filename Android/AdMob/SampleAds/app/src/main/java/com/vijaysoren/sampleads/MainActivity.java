c4f07b9ca0aef6f484695418cd996dd4d54049d8
vijaysoren <37494793+vijaysoren@users.noreply.github.com>
2022-09-29 14:13:38 +0530

    admob interstitial ad added
    new code block for AdMob interstitial ad added

--- Android/AdMob/SampleAds/app/src/main/java/com/vijaysoren/sampleads/MainActivity.java
+++ Android/AdMob/SampleAds/app/src/main/java/com/vijaysoren/sampleads/MainActivity.java
@@ -1,20 +1,31 @@
 package com.vijaysoren.sampleads;
 
 import androidx.appcompat.app.AppCompatActivity;
+package com.vijaysoren.sampleads;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
+import com.google.android.gms.ads.AdError;
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
 
+import com.google.android.gms.ads.FullScreenContentCallback;
+import com.google.android.gms.ads.LoadAdError;
 import com.google.android.gms.ads.MobileAds;
 
 import com.google.android.gms.ads.initialization.InitializationStatus;
 import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
 import com.google.android.gms.ads.interstitial.InterstitialAd;
+import com.google.android.gms.ads.interstitial.InterstitialAdLoadCallback;
 
 public class MainActivity extends AppCompatActivity {
 
+    private InterstitialAd mInterstitialAd;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,9 +37,68 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        //banner ad
         AdView adView = findViewById(R.id.adView);
         AdRequest adRequest = new AdRequest.Builder().build();
         adView.loadAd(adRequest);
 
+        
+        // Interstitial Ad [start]
+        InterstitialAd.load(this, "ca-app-pub-3940256099942544/1033173712", adRequest, new InterstitialAdLoadCallback() {
+            @Override
+            public void onAdLoaded(@NonNull InterstitialAd interstitialAd) {
+                super.onAdLoaded(interstitialAd);
+
+                MainActivity.this.mInterstitialAd = interstitialAd;
+
+                Toast.makeText(MainActivity.this, "Interstitial Ad loaded", Toast.LENGTH_SHORT).show();
+
+                interstitialAd.setFullScreenContentCallback(new FullScreenContentCallback() {
+                    @Override
+                    public void onAdClicked() {
+                        super.onAdClicked();
+                    }
+
+                    @Override
+                    public void onAdDismissedFullScreenContent() {
+                        super.onAdDismissedFullScreenContent();
+
+                        mInterstitialAd = null;
+                    }
+
+                    @Override
+                    public void onAdFailedToShowFullScreenContent(@NonNull AdError adError) {
+                        super.onAdFailedToShowFullScreenContent(adError);
+
+                        mInterstitialAd = null;
+                    }
+
+                    @Override
+                    public void onAdImpression() {
+                        super.onAdImpression();
+                    }
+
+                    @Override
+                    public void onAdShowedFullScreenContent() {
+                        super.onAdShowedFullScreenContent();
+                    }
+                });
+
+                if (mInterstitialAd != null){
+                    interstitialAd.show(MainActivity.this);
+                }else{
+                    Log.d("TAG", "The interstitial ad wasn't ready yet.");
+                }
+            }
+
+            @Override
+            public void onAdFailedToLoad(@NonNull LoadAdError loadAdError) {
+                super.onAdFailedToLoad(loadAdError);
+
+                mInterstitialAd = null;
+            }
+        });
+
+        //interstitial Ad [end]
     }
 }
